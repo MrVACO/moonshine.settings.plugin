084d37566f07e596f4c9a4d5ae12e5bbc8c51446
Mr.VACO <brute1904@gmail.com>
2025-05-26 17:12:57 +0500

    Base files for package

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MrVaco\Moonshine\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingsServiceProvider extends ServiceProvider
+{
+
+}
